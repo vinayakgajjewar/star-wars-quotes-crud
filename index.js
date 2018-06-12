5aa082f7df49b2330777b8b7344488e82fb3e45f
Vinayak Gajjewar <vinayakgajjewar@gmail.com>
2018-06-11 21:56:23 -0700

    Add mongodb support

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+var MongoClient = require("mongodb").MongoClient
 
 var app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
 
 var port = 3000;
+var db;
+
+MongoClient.connect("mongodb://vinayakgajjewar:vinayakgajjewar1@ds016718.mlab.com:16718/star-wars-quotes-db", function(error, client) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Connected to database successfully");
+  }
+
+  if (client != null) {
+    db = client.db("star-wars-quotes-db");
+    app.listen(port, function() {
+      console.log("Listening on port " + port);
+    });
+  } else {
+    console.log("client is null");
+  }
+});
 
 app.get("/", function(request, response) {
   // console.log("GET request at /");
@@ -15,8 +34,4 @@ app.get("/", function(request, response) {
 app.post("/quotes", function(request, response) {
   // console.log("POST request at /quotes");
   console.log(request.body);
-});
-
-app.listen(port, function() {
-  console.log("Listening on port " + port);
 });
